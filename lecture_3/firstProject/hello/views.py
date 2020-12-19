58c8952e8fe903b8ec5aeecf0594e30dbaeb8b96
ani16rs <ani16rs@gmail.com>
2020-12-19 19:03:39 +0530

    completed nweyear, started tasks

--- lecture_3/firstProject/hello/views.py
+++ lecture_3/firstProject/hello/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "hello/index.html")
 
 def anmol(request):
     return HttpResponse("Hello, Anmol")
 
 def anu(request):
     return HttpResponse("Hello, Anu")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+            "name": name.capitalize()
+    })
