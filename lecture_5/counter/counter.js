595eb888da4ee57045de1b7470c07e841ba85e93
ani16rs <ani16rs@gmail.com>
2021-01-26 13:49:53 +0530

    finished JS lecture

--- lecture_5/counter/counter.js
+++ lecture_5/counter/counter.js
@@ -1,13 +1,18 @@
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0)
+}
+
 let counter = 0;
 
 function count() {
+    counter = localStorage.getItem('counter')
     counter++;
     document.querySelector('h1').innerHTML = counter;
-    if(counter % 10 === 0) {
-        alert(`The count is now ${counter}`);
-    }
+    localStorage.setItem('counter', counter)
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
 });
